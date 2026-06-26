6b27a4aa3f56773d7a44cfdfcc2e2b1b2a8bdf7d
Oscar Jauffret <oscarjauffret@gmail.com>
2026-06-26 11:57:42 +0200

    Refactored a bit

--- include/graphics/fixedCamera.hpp
+++ include/graphics/fixedCamera.hpp
@@ -12,7 +12,6 @@
 
 class FixedCamera {
 public:
-    glm::vec3 position;
     glm::vec3 target;
     glm::vec3 worldUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
@@ -21,24 +20,25 @@ public:
     float theta;
 
     FixedCamera(float distance_, glm::vec3 target_, float theta_ = std::numbers::pi/2, float phi_ = std::numbers::pi/3):
-    distance(distance_), phi(phi_), theta(theta_), target(target_) {
-        computeCameraPosition();
-    }
+    distance(distance_), phi(phi_), theta(theta_), target(target_) {}
 
-    glm::mat4 getViewMatrix(glm::vec3 target) {
-        return glm::lookAt(computeCameraPosition(), target, worldUp);
+    glm::mat4 getViewMatrix(glm::vec3 target_) {
+        target = target_;
+        return glm::lookAt(getCameraPosition(), target_, worldUp);
     }
 
 private:
-    glm::vec3 computeCameraPosition() {
-        position = target;
+    glm::vec3 getCameraPosition() {
+        glm::vec3 position = target;
         position.x += distance * cos(theta) * sin(phi);
         position.y += distance * sin(theta) * sin(phi);
         position.x += distance * cos(phi);
+
+        return position;
     }
 
     glm::vec3 getCameraDirection() {
-        return glm::normalize(position - target);
+        return glm::normalize(getCameraPosition() - target);
     }
 
     glm::vec3 getCameraRight() {
